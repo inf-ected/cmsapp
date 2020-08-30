04b94e114bef6d9ee347cff4e45af9c28e83a375
Golicov Alexandr <infarion@gmail.com>
2020-08-30 14:28:35 +0300

    first

---
+++ app/Http/Controllers/StaticPageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\static_page;
+use Illuminate\Http\Request;
+
+class StaticPageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\static_page  $static_page
+     * @return \Illuminate\Http\Response
+     */
+    public function show(static_page $static_page)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\static_page  $static_page
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(static_page $static_page)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\static_page  $static_page
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, static_page $static_page)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\static_page  $static_page
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(static_page $static_page)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_08_30_112138_create_static_pages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaticPagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('static_pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('url');
+            $table->longText('content');
+            $table->boolean('published');
+            $table->timestamps();
+            $table->timestamps('publisged_at');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('static_pages');
+    }
+}
